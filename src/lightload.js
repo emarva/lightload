73b8d2e296e28d624704878fcb82075da5ad30a7
Daniel Nuñez S. <dnunezse@gmail.com>
2014-12-30 14:56:29 -0300

    Comenzando a tomar forma

---
+++ src/lightload.js
@@ -0,0 +1,28 @@
+var lightload = (function() {
+	/*
+		metodos: 
+		
+			lightload.get(
+				{url: 'mi-js.js', name: 'js'},
+				{url: 'mi-css.css, cache: false, expires: 4}
+			);
+			
+			// Si no se le asigna un nombre la url sera el nombre
+			
+			lightload.remove(name);
+			lightload.clear();
+	
+	*/
+	
+	return {
+		get: function(urls) {
+		
+		},
+		remove: function(name) {
+		
+		},
+		clear: function() {
+		
+		}
+	}
+})();
